fe149ad2df6d20a30333cef50b4c7905d9770026
stanislaus_dosemon <sdosemon@gmail.com>
2025-05-01 12:41:05 +1000

    PEP8 Test

--- problem1__Multiples_of_3_or_5.py
+++ problem1__Multiples_of_3_or_5.py
@@ -1,13 +1,16 @@
-#If we list all the natural numbers below 10 
-#that are multiples of 3 or 5, we get 3,5,6, and 9. 
-#The sum of these multiples is 23.
-#Find the sum of all the multiples of 3 or 5 below 1000.
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6, and 9. 
+#The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
-def multiples_of_3n5(number):
+def multiples_of_3n5(max_number):
+    '''
+    '''
+
     lst = []
-    for i in range(number):
-        if i % 3 == 0 or i % 5 == 0:
-            lst.append(i)
+
+    # Check if num is divisible by 3 or 5
+    for num in range(max_number):
+        if num % 3 == 0 or num % 5 == 0:
+            lst.append(num)
     return sum(lst)
 
 print(multiples_of_3n5(1000))
